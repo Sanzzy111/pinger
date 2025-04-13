317c1d8d52e359928433a96d5443841fa4165894
Sanzzy111 <sanboygitu@gmail.com>
2025-04-13 19:51:48 +0700

    server.py

--- server.py
+++ server.py
@@ -1,17 +1,12 @@
 from flask import Flask
-import requests
+import os
 
 app = Flask(__name__)
 
-REPLIT_URL = "https://5b23b15c-265d-4760-bf8b-01f9335f88ce-00-2m3gi84pg9q5z.sisko.repl.co/"
-
 @app.route('/')
 def home():
-    try:
-        requests.get(REPLIT_URL)
-        return "Pinged your Replit bot!"
-    except:
-        return "Failed to ping Replit bot."
+    return "I'm alive!"
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
